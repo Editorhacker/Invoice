ad687b8958ec665b9cfe3a54b6d86a75a1d757cd
Editorhacker <94226999+Editorhacker@users.noreply.github.com>
2025-12-18 14:17:51 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://13.203.105.228:3000",
-  ],
+  /* config options here */
 };
 
 export default nextConfig;
-
